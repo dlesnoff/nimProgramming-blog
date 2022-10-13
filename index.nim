b927a94fbc4207ba699d98c142fddffc2315a6da
Dimitri Lesnoff <dimitri.lesnoff@gmail.com>
2022-10-14 00:17:53 +0200

    Concise sentences & add discard examples & links

--- index.nim
+++ index.nim
@@ -3,15 +3,16 @@ import nimib
 
 nbInit
 
+import std/[macros]
 nbText: """
 # Nim Metaprogramming (Macros) Tutorial
-This tutorial is designed to be a step-by-step introduction to the metaprogramming features of the Nim Language.
-There already many resources on the Web, but I strive to provide more thorough details on the development process and gather all the infos of these different resources.
+This tutorial aims to be a step-by-step introduction to the metaprogramming features of the Nim Language.
+There are already many resources on the Web, but I strive to provide more thorough details on the development process all in one place.
 
 ## Existing resources
-The links provided open in the same tab by default. You surely want to press `Ctrl` while you click, in order to open in a new tab (and not lose this tutorial page in a history mess).
+Press `Ctrl` + `Click` to open following links in a new tab.
 
-First, there are three official resources:
+First, there are three official resources at the Nim's website:
   1. [Nim Tutorial (Part III)](https://nim-lang.org/docs/tut3.html)
   2. [Manual section about macros](https://nim-lang.org/docs/manual.html#macros)
   3. [The Standard Documentation of the std/macros library](https://nim-lang.org/docs/macros.html)
@@ -34,15 +35,22 @@ Last but no least, there are three Nim books:
   2. [Mastering Nim, auto-published by A. Rumpf/Araq, Nim's creator](https://www.amazon.fr/dp/B0B4R7B9YX). (This link is NOT affiliated)
   3. [Nim Programming Book, by S.Salewski](https://ssalewski.de/nimprogramming.html#_macros_and_meta_programming)
 
+We can also count many projects that are macro- or template-based:
+  1. [genny](https://github.com/treeform/genny) and [benchy](https://github.com/treeform/genny). Benchy is a template based library that benchmarks your code snippet under bench blocks. Genny is used to export a Nim library to other languages (C, C++, Node, Python, Zig).
+  In general, treeform projects source code are good Nim references
+  2. My favorite macro : the [neural network domain specific language (DSL) of the tensor library Arraymancer](https://github.com/mratsim/Arraymancer/blob/68786e147a94069a96f069bab327d67afdaa5a3e/src/arraymancer/nn/nn_dsl.nim)
+Do not just look at the header of procedures to determine if it is a macro or not. Often a macro relies on a lot of smaller procedures.
+
 Let us start with introduction to `template` and `untyped` parameters. Another form of metaprogramming is done with `generics`, but these are not explained in the scope of this tutorial.
 """
 
 nbText: """
 ## Templates
-We can see *templates* as procedures that generate code. Usually, it also takes code as parameters. The input code is always given as an `untyped` parameter.
-Templates are designed to be an efficient copy-paste mechanism, very like `#define` macros in C language family (C++, C# as well).
-We can add an (already existing in fact) boolean operator, to simplify expressions.
-Notice we use *stropping* to overwrite the already existing operator.
+We can see *templates* as procedures that modify code through a copy-paste mechanism. Pieces of code are given to (and outputted by) the template with a special type : `untyped`. 
+For those familiar with [preprocessing](https://gcc.gnu.org/onlinedocs/cpp/) in the C family of languages (C, C++, C#), it does the same than the `#define` or `#if`, `#endif` macros and much more.
+"""
+nbText: """
+Nim's language defines boolean operator like `!=` with templates. You can even look at Nim's source code, that's almost the same code. See the [documentation](https://nim-lang.org/docs/system.html#%21%3D.t%2Cuntyped%2Cuntyped).
 """
 nbCode:
   ## Example from std/manual
@@ -53,6 +61,7 @@ nbCode:
 
 nbText: """
 We can easily repeat code under a custom block. Here duplicate, just duplicate code and repeat takes an additional parameter, an int, as a generalisation of the `duplicate` template.
+Notice that duplicate is not smart. It will repeat any assignment twice in the code's block.
 """
 
 nbCode:
@@ -94,16 +103,26 @@ nbCode:
         myInt = myInt div 2
 
 nbText:"""
+We can not do certain things with template, like not returning any code.
+"""
+
+nbCode:
+  template `discard`(statements: untyped): untyped =
+    discard nil # Cheat here
+
+  `discard`:
+    echo "Test the discard template, if you see this message, it has failed. If not, well …"
+
+nbText:"""
 ## Macros
 Macros can be seen as an empowered template procedure.
-Instead of simply taking untyped blocks and reuse them as lego bricks to return a code, we can parse our untyped parameter and do something conditionnaly to informations given in these parameters.
+Instead of simply taking untyped blocks and reuse them as lego bricks to return a code, we can parse our untyped parameter and do something conditionally to informations given in these parameters.
 """
-# nbCode:
-#   template `discard`(statements: untyped) =
-#     result = nil
-
-#   `discard`:
-#     while true:
-#       echo "If you do not discard me, you'll never get out!"
+nbCode:
+  macro discard2(statements: untyped): untyped =
+    result = newStmtList()
+  discard2:
+    while true:
+      echo "If you do not discard me, you'll never get out!"
 
 nbSave
