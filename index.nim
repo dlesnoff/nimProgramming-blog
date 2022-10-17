0306df070659e24b12552537b26d953f37609446
Dimitri Lesnoff <dimitri.lesnoff@gmail.com>
2022-10-17 23:11:32 +0200

    Add my first macro (memory layout of a type)

--- index.nim
+++ index.nim
@@ -41,7 +41,14 @@ We can also count many projects that are macro- or template-based:
   2. My favorite macro : the [neural network domain specific language (DSL) of the tensor library Arraymancer](https://github.com/mratsim/Arraymancer/blob/68786e147a94069a96f069bab327d67afdaa5a3e/src/arraymancer/nn/nn_dsl.nim)
 Do not just look at the header of procedures to determine if it is a macro or not. Often a macro relies on a lot of smaller procedures.
 
-Let us start with introduction to `template` and `untyped` parameters. Another form of metaprogramming is done with `generics`, but these are not explained in the scope of this tutorial.
+## Introduction
+There are four kind/levels of procedures:
+  1. ordinary proc/iterator
+  2. generic proc/iterator
+  3. template
+  4. macro
+The higher the number, the more meta we get. It is recommended to program one's procedure with the lowest level of metaprogramming possible. 
+Let us start with `template`s and `untyped` parameters. I do not `present` generics in this tutorial.
 """
 
 nbText: """
@@ -81,13 +88,21 @@ nbCode:
 
   repeat 5:
     echo("Hello Templates!")
-    sleep(1000)
+    sleep(100)
 
 nbText: """
-In standard Nim, there are very few restricted keywords and special control-flow mechanisms, but nothing restrains us from creating a doWhile construct. And this is very simple.
+In Nim, there are few restricted keywords and special control-flow mechanisms, as to incite us to create our own constructs (and keep the language simple). Nothing restrains us from defining a `doWhile` construct.
 """
 
 nbCode:
+  proc nextSyracuseTerm(term: var int) =
+    ## This sequence should resolve to the cyclic sequence 1, 4, 2, 1, ...
+    if term mod 2 == 1:
+      term *= 3
+      term.inc
+    else:
+      term = term div 2
+
   template doWhile(conditional, loop: untyped) =
     loop
     while conditional:
@@ -96,14 +111,28 @@ nbCode:
   var myInt = 5
   doWhile myInt >= 6:
       echo myInt
-      if myInt mod 2 == 1:
-        myInt *= 3
-        myInt.inc
-      else:
-        myInt = myInt div 2
+      nextSyracuseTerm(myInt)
 
 nbText:"""
-We can not do certain things with template, like not returning any code.
+Notice though that the end result is fairly different than C++ code
+```cpp
+int i = 0;
+do{
+  printf("Hello World");
+  i += 1;
+}while(i < 10);
+```
+The instructions and the conditional appear before the `while` whereas this doWhile Nim construct will have the conditional after the `doWhile`:
+```nim
+var i: int = 0 # superfluous type annotation
+doWhile i < 10:
+  echo "Hello World!"
+  i.inc
+```
+"""
+  
+nbText:"""
+Though powerful, templates are still limited. As an example, I didn't achieve to make a discard template (that ta disabling a code.
 """
 
 nbCode:
@@ -125,4 +154,168 @@ nbCode:
     while true:
       echo "If you do not discard me, you'll never get out!"
 
+nbText:"""
+### AST Manipulation
+In Nim, the code is read and transformed in an internal intermediate representation called an Abstract Syntax Tree (AST) (refer to the [manual AST section]()
+"""
+
+nbCode:
+  dumpTree:
+    type
+      myObject {.packed.} = ref object of RootObj
+        left: seq[myObject]
+        right: seq[myObject]
+nbText:"""
+```raw
+StmtList
+  TypeSection
+    TypeDef
+      PragmaExpr
+        Ident "myObject"
+        Pragma
+          Ident "packed"
+      Empty
+      RefTy
+        ObjectTy
+          Empty
+          OfInherit
+            Ident "RootObj"
+          RecList
+            IdentDefs
+              Ident "left"
+              BracketExpr
+                Ident "seq"
+                Ident "myObject"
+              Empty
+            IdentDefs
+              Ident "right"
+              BracketExpr
+                Ident "seq"
+                Ident "myObject"
+              Empty
+```
+"""
+
+nbText:"""
+I present down my first macro as an example.
+I want to print the memory layout of a given type.
+My goal is to find misaligned fields making useless unocuppied memory.
+We should be able in the future to automatically swap the fields to remove the holes (or at least propose a fixed type).
+The first step is to look at the AST of the input code we want to parse.
+"""
+
+nbCode:
+  dumpTree:
+    type
+      Thing = object
+        a: float32
+
+nbText:"""
+It is not easy (if even possible) to list all the possible types.
+By adding some other informations we get a better picture of the general AST of a type.
+"""
+
+nbCode:
+  dumpTree:
+    type
+      Thing {.packed.} = object of RootObj
+        a: float32
+        b: string
+
+nbText:"""
+A macro does three steps in this order:
+  0. Check that the node is of the expected kind.
+  1. Get properties of the input AST node (Maybe by going through node child's Ident).
+  2. Form AST output in function of these input node's properties.
+  3. Go through the node's child.
+"""
+
+nbText:"""
+A good macro is necessary complex enough to require a long docstring with thorough details.
+We begin by running the type definition.
+"""
+
+nbCode:
+  macro typeMemoryRepr(typedef: untyped): untyped =
+    ## This macro takes a type definition as an argument and:
+    ## * defines the type (outputs typedef as is)
+    ## * initializes a variable of this type
+    ## * echoes the size of the variable
+    ## * echoes the address of the variable
+    ## Then, for each field:
+    ## * echoes the size of the variable field
+    ## * echoes the address of the variable field
+
+    # Parse the type definition to find the TypeDef section's node
+    result = quote do:
+      `typedef`
+    for statement in typedef:
+      if statement.kind == nnkTypeSection:
+        let typeSection = statement
+        for i in 0 ..< typeSection.len:
+          if typeSection[i].kind == nnkTypeDef:
+            var tnode = typeSection[i]
+            # The name of the type is the first Ident child. We can get the ident's string with strVal or repr
+            let nameOfType = typeSection[i].findChild(it.kind == nnkIdent)
+
+            ## Generation of AST:
+            # We create a variable of the given type definition (hopefully not already defined) name for the "myTypenameVar"
+            let nameOfTestVariable = "my" & nameOfType.strVal.capitalizeAscii() & "Var"
+            let testVariable = newIdentNode(nameOfTestVariable)
+            result = result.add(
+            quote do:
+              var `testVariable`:`nameOfType` # instanciate type defined in typedef
+              echo `testVariable`.sizeof # echo the total size
+              echo `testVariable`.addr.repr # gives the address in memory
+            )
+            # myTypeVar.field[i] memory size and address in memory
+            tnode = tnode[2][2] # The third child of the third child is the fields's AST
+            assert tnode.kind == nnkRecList
+            for i in 0 ..< tnode.len:
+              # myTypeVar.field[i].sizeof
+              result = result.add(newStmtList(nnkCommand.newTree(
+                newIdentNode("echo"),
+                nnkDotExpr.newTree(
+                  nnkDotExpr.newTree(
+                    newIdentNode(nameOfTestVariable),
+                    newIdentNode(tnode[i][0].strVal) # The name of the field is the first ident
+                    ),
+                    newIdentNode("sizeof")
+                )
+                )))
+              # myTypeVar.field[i].addr.repr
+
+              result = result.add(newStmtList(nnkCommand.newTree(
+                newIdentNode("echo"),
+                nnkDotExpr.newTree(
+                  nnkDotExpr.newTree(
+                    nnkDotExpr.newTree(
+                      newIdentNode(nameOfTestVariable),
+                      newIdentNode(tnode[i][0].strVal)
+                    ),
+                    newIdentNode("addr")
+                  ),
+                  newIdentNode("repr")
+                )
+                )))
+    echo result.repr
+
+  when isMainModule:
+    typeMemoryRepr:
+      type
+        Thing = object of RootObj
+          a: float32
+          b: uint64
+          c: char
+      type
+        Thing2 = object
+          oneChar: char
+          myStr: string
+    when false: # erroneous code
+      # type with pragmas aren't supported yet
+      typeMemoryRepr:
+        type
+          Thing {.packed.} = object
+            oneChar: char
+            myStr: string
 nbSave
