caea9850bb040ac10116815ad7dc2cf63f289296
Dimitri LESNOFF <dimitri.lesnoff@lip6.fr>
2023-10-29 20:31:37 +0100

    Add two scripts to automate the AST pictures generation

---
+++ blogPosts/astToGraphviz_pmunch.nim
@@ -0,0 +1,38 @@
+import strutils
+
+var
+  nodeName = "a"
+  lastIndent = 0
+  parents: seq[string]
+
+var test: string = readAll(open("ASTtree.txt"))
+test = test.strip(chars= {'\n'}) # Remove closing end of line
+
+echo "digraph {"
+for line in test.splitLines:
+  if parents.len == 0:
+    echo nodeName, "[label=\"", line.strip, "\", shape=rect]"
+  else:
+    var thisIndent = (line.len - line.strip(trailing = false, chars = {' '}).len) div 2
+    if thisIndent == lastIndent:
+      parents.setLen(parents.len - 1) # Discard old non-parent node
+    elif thisIndent < lastIndent:
+      parents.setLen(parents.len - 1 - (lastIndent - thisIndent)) # Discard old non-parent node and last parent
+    lastIndent = thisIndent
+    echo nodeName, "[label=\"", line.strip.replace("\"", "'"), "\", shape=rect]"
+    echo parents[parents.high], " -> ", nodeName
+  parents.add nodeName
+
+  var pos = nodeName.high
+  while true:
+    nodeName[pos] = if nodeName[pos] == 'z': 'a' else: chr(nodeName[pos].ord + 1)
+    if nodeName[pos] == 'a':
+      dec pos
+      if pos == -1:
+        nodeName.add 'a'
+        break
+    else:
+      break
+for parent in 0..<parents.high:
+  echo parents[parent], "[style=filled]"
+echo "}"
---
+++ blogPosts/astToTikz_vindaar.nim
@@ -0,0 +1,53 @@
+import strutils
+const tmpl = """
+\documentclass{standalone}
+\usepackage{tikz}
+\usetikzlibrary{trees}
+\begin{document}
+\tikzstyle{every node}=[draw=black, rectangle, fill=teal]
+\tikzstyle{Ident}=[thick, fill=orange]
+\begin{tikzpicture}[%
+  sibling distance=25mm,
+  % TODO: think about sibling dstances
+  edge from parent fork down
+]
+$#
+\end{tikzpicture}
+\end{document}
+"""
+
+import macros
+proc removePrefix(s, p: string): string =
+  result = s
+  result.removePrefix(p)
+proc node(n: NimNode): string =
+  let nK = n.kind.repr.removePrefix("nnk")
+  case n.kind
+  of nnkIdent: "node [Ident] {\"" & n.strVal & "\"} "
+  else:"node {" & nK & "} "
+proc child(s: string): string = "child { $# } " % s
+proc child(n: NimNode): string =
+  let nStr = n.repr
+  result = child(nStr)
+
+proc astToTikz(n: NimNode): string =
+  case n.kind
+  of nnkLiterals, nnkIdent, nnkSym:
+    result = node(n)
+  else:
+    result = node(n)
+    for ch in n:
+      result.add child(astToTikz(ch))
+
+macro toTikZ(n: untyped): untyped =
+  let data = "\\" & astToTikz(n) & ";"
+  result = newLit(data)
+
+let str = toTikZ:
+  type
+    myObject {.packed.} = ref object of RootObj
+      left: seq[myObject]
+      right: seq[myObject]
+
+import latexdsl # to compile directly
+compile("./tikz_type.tex", str, tmpl)
